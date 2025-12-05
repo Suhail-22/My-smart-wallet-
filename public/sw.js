293ddaf15111581058b7e3ee3c8d03f96137d6ea
Suhail-22 <101893675+Suhail-22@users.noreply.github.com>
2025-12-05 15:43:04 +0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,22 @@
-// Service Worker بسيط
+// service worker بسيط للتثبيت
 const CACHE_NAME = 'smart-wallet-v1';
-const urlsToCache = ['/', '/index.html'];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
